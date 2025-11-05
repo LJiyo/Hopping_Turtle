2b69b5f2e93c6ee282fd54c2b8967e2c73ce3d2c
Lars Bosales <larsjohanbosales@gmail.com>
2025-11-05 14:56:40 +1300

    Refactor: Corrected Expected Output from Failed Case

--- hopping-turtle/__tests__/eta.test.ts
+++ hopping-turtle/__tests__/eta.test.ts
@@ -3,7 +3,7 @@ import estimate from "../utils/ETACalculator";
 
 test("calculates ETA based on remaining distance = 20 (meter), speed = 2", () => {
   const eta = ETACalculator.estimate(20, 2);
-  expect(eta).toBe(10); // 20/2 = 10
+  expect(eta).toBe("0.2 min(s) to arrival"); // 20/2 = 10, 10/60 = 0.16 -> 0.2 rounded
 });
 
 test("No reading given if speed is less than 0.5m/s", () => {
